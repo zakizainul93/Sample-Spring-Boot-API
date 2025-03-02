1dfb8d36f7147e81c62ed0d9b41410fac0a52dc6
zaki <zaki@dnb.com.my>
2025-03-02 23:08:02 +0800

    Base structure

---
+++ src/test/java/com/example/Sample_API/SampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sample_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
